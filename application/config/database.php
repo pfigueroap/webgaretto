d48f61b43bb10bf494eedf232655f50a138df6bd
Pablo <pablo@debweb.bprog.cl>
2017-07-26 19:07:30 -0400

    cambio en archivo DB

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = '081283';
 $db['default']['database'] = 'garetto';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
